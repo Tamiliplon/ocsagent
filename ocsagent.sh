e27d1d406c9c66ca98453667417ffedd5eee0b61
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2023-04-14 18:01:00 +0530

    Update ocsagent.sh

--- ocsagent.sh
+++ ocsagent.sh
@@ -1,4 +1,4 @@
-cd /home/ubuntu/repo/
+cd /home/iplon/repos_1/
 wget https://github.com/OCSInventory-NG/UnixAgent/releases/download/v2.9.3/Ocsinventory-Unix-Agent-2.9.3.tar.gz
 sudo apt update
 sudo apt install -y libnet-ssleay-perl libnet-ip-perl libnet-netmask-perl libnet-snmp-perl libproc-daemon-perl libproc-pid-file-perl libxml-simple-perl make nmap
