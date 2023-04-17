57d2951afea367508217651a1bf5d069a1e71362
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2023-04-17 17:00:05 +0530

    Update ocsagent.sh

--- ocsagent.sh
+++ ocsagent.sh
@@ -5,7 +5,10 @@ sudo apt install -y libnet-ssleay-perl libnet-ip-perl libnet-netmask-perl libnet
 tar xf Ocsinventory-Unix-Agent-2.9.3.tar.gz
 cd Ocsinventory-Unix-Agent-2.9.3/
 PERL_AUTOINSTALL=1 perl Makefile.PL
-sudo ./ocsinventory-agent --devlib --server https://ocs.iplon.co.in/ocsinventory --tag newtesting --CA /etc/ssl/certs/ca-certificates.crt
+sudo ./ocsinventory-agent --devlib --server http://54.250.208.236/ocsinventory --tag ocsansiblenode
+
+
+#sudo ./ocsinventory-agent --devlib --server https://ocs.iplon.co.in/ocsinventory --tag newtesting --CA /etc/ssl/certs/ca-certificates.crt
 
 #sudo ./ocsinventory-agent --devlib --server http://35.200.223.70/ocsinventory
 
